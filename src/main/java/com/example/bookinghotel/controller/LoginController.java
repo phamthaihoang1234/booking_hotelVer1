2ee38dd8d331017b21f29111e2fb785831c1dfd3
Dung HE150755 <dungtnthe150755@fpt.edu.vn>
2022-02-07 20:30:09 +0700

    cover cac case cua nut save login

--- src/main/java/com/example/bookinghotel/controller/LoginController.java
+++ src/main/java/com/example/bookinghotel/controller/LoginController.java
@@ -19,63 +19,87 @@ import java.util.Optional;
 public class LoginController {
 
 
-
     @Autowired
     private UserService userService;
 
 
-    @RequestMapping(value = "/login", method = { RequestMethod.GET, RequestMethod.POST })
-    public void handleLogin(@RequestParam("username")String username , @RequestParam("password") String password, @RequestParam("savelogin") Boolean savelogin, HttpServletRequest request, HttpServletResponse response ) throws IOException {
+    @RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
+    public void handleLogin(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("savelogin") Boolean savelogin, HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         try (PrintWriter out = response.getWriter()) {
 
 
-        Optional<UserInfo> user =  userService.findByUserName(username);
-        if(user.isPresent()){
-            UserInfo userInfo = user.get();
-            // xử lý cả 2 trường hợp password bị mã hóa trên cookie và password chưa encrypt trong so sánh psw
-            if(userInfo.getPassword().equals(password)||UserInfo.getPasswordEncoder().matches(password,userInfo.getPassword())){
-                // kiểm tra cookie về tài khoản yoy tồn tại chưa
-                boolean isDuplicated = false;
-                if(savelogin==true){
-                    Cookie[] cookies = request.getCookies();
-                    for(int i = 0 ;i<cookies.length;i++){
-                        if(cookies[i].getName().equals("yoyuser")) {
-                            cookies[i].setValue(userInfo.getUsername());
-                            cookies[i].setMaxAge(31536000);
-                            isDuplicated = true;
-                            response.addCookie(cookies[i]);
+            Optional<UserInfo> user = userService.findByUserName(username);
+            if (user.isPresent()) {
+                UserInfo userInfo = user.get();
+                // xử lý cả 2 trường hợp password bị mã hóa trên cookie và password chưa encrypt trong so sánh psw
+                if (userInfo.getPassword().equals(password) || UserInfo.getPasswordEncoder().matches(password, userInfo.getPassword())) {
+
+                    // xử lý lưu cookie
+                    if (savelogin == true) {
+                        // kiểm tra cookie về tài khoản yoy tồn tại chưa
+                        Cookie cookieU = findCookie(request, "yoyuser");
+                        Cookie cookieP = findCookie(request, "yoypsw");
+                        if (cookieU != null) {
+                            cookieU.setValue(userInfo.getUsername());
+                            cookieU.setMaxAge(31536000);
+                            response.addCookie(cookieU);
+                        } else {
+                            Cookie newCookie1 = new Cookie("yoyuser", userInfo.getUsername());
+                            newCookie1.setMaxAge(31536000); // lưu cookie 1 năm
+                            response.addCookie(newCookie1);
                         }
-                        if(cookies[i].getName().equals("yoypsw")){
-                            cookies[i].setValue(userInfo.getPassword());
-                            cookies[i].setMaxAge(31536000);
-                            response.addCookie(cookies[i]);
+
+                        if (cookieP != null) {
+                            cookieP.setValue(userInfo.getPassword());
+                            cookieP.setMaxAge(31536000);
+                            response.addCookie(cookieP);
+                        } else {
+                            Cookie newCookie2 = new Cookie("yoypsw", userInfo.getPassword());
+                            newCookie2.setMaxAge(31536000);
+                            response.addCookie(newCookie2);
+                        }
+
+
+                    } else {
+                        //xử lý cookie đã lưu nhưng k muốn lưu nữa(bằng cách đăng nhập mà k chọn save login
+                        Cookie cookieU = findCookie(request, "yoyuser");
+                        Cookie cookieP = findCookie(request, "yoypsw");
+                        if (cookieU != null) {
+                            cookieU.setMaxAge(0);
+                            response.addCookie(cookieU);
+                        }
+                        if (cookieP != null) {
+                            cookieP.setMaxAge(0);
+                            response.addCookie(cookieP);
                         }
                     }
-                    if(isDuplicated==false){
-                        Cookie newCookie1 = new Cookie("yoyuser",userInfo.getUsername());
-                        Cookie newCookie2 = new Cookie("yoypsw",userInfo.getPassword());
-                        newCookie1.setMaxAge(31536000); // lưu cookie 1 năm
-                        newCookie2.setMaxAge(31536000);
-                        response.addCookie(newCookie1);
-                        response.addCookie(newCookie2);
-                    }
+
+                    out.write("Success");
+                } else {
+                    out.write("Wrong password");
                 }
-                out.write("Success");
-            }else{
-                out.write("Wrong password");
+
+
+            } else {
+                out.write("this user doesnt exist");
             }
 
 
-        }else{
-            out.write("this user doesnt exist");
         }
 
 
+    }
+
+    // tim cookie theo ten
+    private Cookie findCookie(HttpServletRequest request, String cookie_name) {
+        Cookie[] cookies = request.getCookies();
+        for (int i = 0; i < cookies.length; i++) {
+            if (cookies[i].getName().equals(cookie_name)) {
+                return cookies[i];
+            }
+
         }
-
-
-
-
+        return null;
     }
 }
