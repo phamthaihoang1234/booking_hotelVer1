3f94c745047587f2becf171fa18d54105720f301
Dung HE150755 <dungtnthe150755@fpt.edu.vn>
2022-02-11 21:06:30 +0700

    fix address_of_hotel

--- src/main/java/com/example/bookinghotel/repositories/HotelRepository.java
+++ src/main/java/com/example/bookinghotel/repositories/HotelRepository.java
@@ -9,7 +9,7 @@ public interface HotelRepository extends CrudRepository<Hotel, Long> {
 //    phan dung code
     @Query(nativeQuery = true,value = "select Hotel.* from Hotel left join Hotel_Property " +
             "on Hotel_Property_id = Hotel_property.id " +
-            "where hotel.hotel_address LIKE CONCAT('%',?1,'%') and Hotel_Property.type LIKE CONCAT('%',?2,'%') ")
+            "where hotel.address_of_hotel LIKE CONCAT('%',?1,'%') and Hotel_Property.type LIKE CONCAT('%',?2,'%') ")
     Iterable<Hotel> findByHotel_addressContainingAndHotel_property(String contains,String hotelProperty);
 // phan dung code-end
 }
