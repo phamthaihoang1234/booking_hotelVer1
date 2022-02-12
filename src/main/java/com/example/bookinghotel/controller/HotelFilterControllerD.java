6469643db388ec93df8ed594d1cea7c0e4b98af8
Dung HE150755 <dungtnthe150755@fpt.edu.vn>
2022-02-13 00:10:07 +0700

    xu ly get message filter

--- src/main/java/com/example/bookinghotel/controller/HotelFilterControllerD.java
+++ src/main/java/com/example/bookinghotel/controller/HotelFilterControllerD.java
@@ -18,25 +18,25 @@ public class HotelFilterControllerD {
     String HotelFiler(){
         return "Pages/search-hotelsD";
     }
-    @PostMapping("/filterData")
-    void FilterData(@RequestParam("location") String location,@RequestParam("price") String price
-            ,@RequestParam("hotel_standard") int standard,@RequestParam("hotel_property") String[] property
-            ,@RequestParam("orderByPrice") String orderByPrice,@RequestParam("orderByStandard") String orderByStandard){
-        String location_processed = "";
-        // xu ly cac thong tin dau vao
-        if(location!=null){
-            location = Normalizer.normalize(location, Normalizer.Form.NFD);
-            location = location.replaceAll("[^\\p{ASCII}]", "");
-            location_processed = location.replaceAll("\\p{M}", "");
-            System.out.println("location sau process:"+location_processed);
-        }else{
+
+    @PostMapping("/testModelAndView")
+    public void XuLy(){
+//        String location_processed = "";
+//        // xu ly cac thong tin dau vao
+//        if(location!=null){
+//            location = Normalizer.normalize(location, Normalizer.Form.NFD);
+//            location = location.replaceAll("[^\\p{ASCII}]", "");
+//            location_processed = location.replaceAll("\\p{M}", "");
+//            System.out.println("location sau process:"+location_processed);
+//        }else{
             // check thong tin co duoc gui toan ven khong
-            System.out.println("Thieu thong tin");
-        }
+
+//        }
+        System.out.println("Thieu thong tin");
 
 
     }
-    @PostMapping("/testModelAndView")
+    @PostMapping("/testModelAndView2")
     public ModelAndView testReturnModelAndView(){
         ModelAndView mv = new ModelAndView("Pages/Hotel_ListDForFilter");
 
