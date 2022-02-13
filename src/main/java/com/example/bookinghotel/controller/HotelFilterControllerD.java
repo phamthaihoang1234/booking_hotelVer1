a6c645b9c3ac44122dff96f78287d64d5836e94e
Dung HE150755 <dungtnthe150755@fpt.edu.vn>
2022-02-13 16:41:24 +0700

    loai bo dau tieng viet

--- src/main/java/com/example/bookinghotel/controller/HotelFilterControllerD.java
+++ src/main/java/com/example/bookinghotel/controller/HotelFilterControllerD.java
@@ -6,7 +6,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.text.Normalizer;
+import java.util.regex.Pattern;
 
 @Controller
 public class HotelFilterControllerD {
@@ -15,29 +21,48 @@ public class HotelFilterControllerD {
     // CONFLICT KHI MERGE CODE
     // phan dung code
     @GetMapping("/search-hotels")
-    String HotelFiler(){
-        return "Pages/search-hotelsD";
+    String HotelFiler() {
+        return "Pages/hotelFilter";
     }
 
-    @PostMapping("/testModelAndView")
-    public void filterProcess(){
-//        String location_processed = "";
-//        // xu ly cac thong tin dau vao
-//        if(location!=null){
-//            location = Normalizer.normalize(location, Normalizer.Form.NFD);
-//            location = location.replaceAll("[^\\p{ASCII}]", "");
-//            location_processed = location.replaceAll("\\p{M}", "");
-//            System.out.println("location sau process:"+location_processed);
-//        }else{
-            // check thong tin co duoc gui toan ven khong
+    @PostMapping("/filterProcess")
+    public void filterProcess(HttpServletResponse response, @RequestParam("location") String location) throws UnsupportedEncodingException {
+//        , @RequestParam("price") String price
+//            , @RequestParam("hotel_standard") String standard, @RequestParam("hotel_property") String[] property
+//            , @RequestParam("orderByPrice") String orderByPrice, @RequestParam("orderByStandard") String orderByStandard
+        String location_processed = "";
+        // xu ly cac thong tin dau vao
+        if (location != null) {
 
-//        }
-        System.out.println("Thieu thong tin");
-
+            System.out.println("location truoc process:" + location);
+            // loai bo dau tieng viet
+            String nfdNormalizedString = Normalizer.normalize(location, Normalizer.Form.NFD);
+            Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+            location_processed = pattern.matcher(nfdNormalizedString).replaceAll("");
+            location_processed = UnExpectedVietnameseChar(location_processed);
+            System.out.println("location sau process:" + location_processed);
+        } else {
+//             check thong tin co duoc gui toan ven khong
+            System.out.println("Thieu thong tin");
+        }
+        response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.write(location_processed );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+    private String UnExpectedVietnameseChar(String str){
+
+        str = str.replace('Đ','D');
+        str = str.replace('đ','d');
+        return str;
+    }
+
     @PostMapping("/testModelAndView2")
-    public ModelAndView testReturnModelAndView(){
+    public ModelAndView testReturnModelAndView() {
         ModelAndView mv = new ModelAndView("Pages/Hotel_ListDForFilter");
 
         return mv;
