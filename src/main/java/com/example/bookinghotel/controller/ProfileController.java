48f3cb85e44eb6e5b0c62666b4ec01feb6910233
iamnxl1798 <quangnhhe21091999@gmail.com>
2022-02-13 15:02:58 +0700

    quangnh 13022022 create profile

--- src/main/java/com/example/bookinghotel/controller/ProfileController.java
+++ src/main/java/com/example/bookinghotel/controller/ProfileController.java
@@ -1,13 +1,17 @@
 package com.example.bookinghotel.controller;
 
+import com.example.bookinghotel.entities.UserInfo;
 import com.example.bookinghotel.repositories.UserRepository;
 import com.example.bookinghotel.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -17,37 +21,45 @@ public class ProfileController {
     @Autowired
     private UserRepository userRepository;
 
-//    @PutMapping("/updateProfile/{id}")
-//    public ResponseEntity<ResponseObject> updateProfile(@RequestBody UserInfo userInfo, @PathVariable Long id) {
-//        UserInfo optionalUserInfo = userRepository.findById(id).map(user -> {
-//            user.setEmail(userInfo.getEmail());
-//            user.setGender(userInfo.getGender());
-//            user.setName(userInfo.getName());
-//            user.setPhoneNumber(userInfo.getPhoneNumber());
-//            user.setAddress(userInfo.getAddress());
-//            user.setEmail(userInfo.getEmail());
-//            return userRepository.save(user);
-//        }).orElseGet(() -> {
-//            userInfo.setId(id);
-//            return userRepository.save(userInfo);
-//        });
-//        return ResponseEntity.status(HttpStatus.OK).body(
-//                new ResponseObject("Ok", "Update profile successfull", userInfo);
-//        );
-//    }
+    private UserInfo userInfo;
+    private UserInfo oldUserInfo;
 
     @GetMapping("/GetDetailsProfile")
     public String goToProfile(HttpServletRequest request, Model model){
-//        Cookie[] cookies= request.getCookies();
-//        for (int i = 0; i < cookies.length; i++) {
-//            System.out.println("ClasssssTesssss: "+cookies[i].getValue());
-//        }
         // id=5
-
-//        model.addAttribute("hotel",user.findById(id).get());
-        return "Pages/profile";
+        model.addAttribute("userInfo",userService.findByUserName(this.getPrincipal()));
+        return "Pages/profile2";
 
     }
 
+    @PostMapping("/saveEditProfile")
+    public String updateProfile(@ModelAttribute UserInfo userInfo){
+        UserInfo oldUserInfo = userService.findByUserName(this.getPrincipal());
+
+        oldUserInfo.setName(userInfo.getName());
+        oldUserInfo.setEmail(userInfo.getEmail());
+        oldUserInfo.setPhoneNumber(userInfo.getPhoneNumber());
+        oldUserInfo.setAddress(userInfo.getAddress());
+        System.out.println("Class: ProfileControler | Method: updateProfile |  " + oldUserInfo.getName());
+        try {
+            userService.save(oldUserInfo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/homepageHotel";
+    }
+
+    private String getPrincipal(){
+        String userName = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            userName = ((UserDetails)principal).getUsername();
+
+        } else {
+            userName = principal.toString();
+        }
+        return userName;
+    }
 
 }
