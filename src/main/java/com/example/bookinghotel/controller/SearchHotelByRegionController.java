ad10c6a317bc2a534b8842e938dd1556609f79ec
Dung HE150755 <dungtnthe150755@fpt.edu.vn>
2022-02-13 14:31:32 +0700

    xong all-locations.html

--- src/main/java/com/example/bookinghotel/controller/SearchHotelByRegionController.java
+++ src/main/java/com/example/bookinghotel/controller/SearchHotelByRegionController.java
@@ -18,7 +18,7 @@ public class SearchHotelByRegionController {
     void returnRegion(HttpServletResponse response){
        String regions = "A,An Giang,B,Bà Rịa - Vũng Tàu,Bạc Liêu,Bắc Giang,Bắc Kạn,Bắc Ninh,Bến Tre,Bình Dương,Bình Định,Bình Phước,Bình Thuận," +
                "C,Cà Mau,Cao Bằng,Cần Thơ,Đ,Đà Nẵng,Đắk Lắk,Đắk Nông,Điện Biên,Đồng Nai,Đồng Tháp,G,Gia Lai," +
-               "H,Hà Giang,Hà Nam,Hà Nội,Hà Tĩnh,Hải Dương,Hải Phòng,Hậu Giang,Hòa Bình,Hưng Yên,K,Kiên Giang,Kon Tum,Khánh Hòa" +
+               "H,Hà Giang,Hà Nam,Hà Nội,Hà Tĩnh,Hải Dương,Hải Phòng,Hậu Giang,Hòa Bình,Hưng Yên,K,Kiên Giang,Kon Tum,Khánh Hòa," +
                "L,Lai Châu,Lạng Sơn,Lào Cai,Lâm Đồng,Long An,N,Nam Định,Ninh Bình,Ninh Thuận,Nghệ An,P,Phú Thọ,Phú Yên," +
                "Q,Quảng Bình,Quảng Nam,Quảng Ninh,Quảng Ngãi,Quảng Trị,S,Sóc Trăng,Sơn La," +
                "T,Tây Ninh,Tiền Giang,TP Hồ Chí Minh,Tuyên Quang,Thái Bình,Thái Nguyên,Thanh Hóa,Thừa Thiên Huế,Trà Vinh,V," +
