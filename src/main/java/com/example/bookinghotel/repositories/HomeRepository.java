03490fef5f7f9fb2850859bff68e766331d1b19e
iamnxl1798 <quangnhhe21091999@gmail.com>
2022-02-22 08:10:07 +0700

    edit html profile

--- src/main/java/com/example/bookinghotel/repositories/HomeRepository.java
+++ src/main/java/com/example/bookinghotel/repositories/HomeRepository.java
@@ -20,9 +20,7 @@ public interface HomeRepository extends CrudRepository<Room, Long> {
     @Query(value = "SELECT * FROM rooms where address like ?1", nativeQuery = true)
     Iterable<Room> findAllRoomByAddress(String add);
 
-    //    @Query("SELECT h.id, h.name, h.description, h.address, h.pricePerNight, h.numBedrooms, h.numBathrooms FROM Home h WHERE h.id = ?1")
-//    @Query(value = "SELECT id, name, description, address, price_per_night, total_of_bedroom, total_of_bathroom, status,province_id, property_type, user_id FROM rooms WHERE id = ?1", nativeQuery = true)
-    @Query(value = "SELECT * FROM rooms WHERE id = ?1", nativeQuery = true)
+     @Query(value = "SELECT * FROM rooms WHERE id = ?1", nativeQuery = true)
     Optional<Room> findByHomeId(Long id);
 
     @Query(nativeQuery = true,value = "call GetLastRoom")
