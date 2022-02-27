41926653032d0b65045b851808d185f227bec54e
Dung HE150755 <dungtnthe150755@fpt.edu.vn>
2022-02-27 18:43:01 +0700

    cap nhat so luong phong trong

--- src/main/java/com/example/bookinghotel/controller/HotelDetailDController.java
+++ src/main/java/com/example/bookinghotel/controller/HotelDetailDController.java
@@ -1,9 +1,7 @@
 package com.example.bookinghotel.controller;
 
-import com.example.bookinghotel.entities.Booking;
-import com.example.bookinghotel.entities.Hotel;
-import com.example.bookinghotel.entities.Room;
-import com.example.bookinghotel.entities.RoomGroup;
+import com.example.bookinghotel.entities.*;
+import com.example.bookinghotel.repositories.PropertyTypeRepository;
 import com.example.bookinghotel.services.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,106 +20,159 @@ import java.util.*;
 public class HotelDetailDController {
     @Autowired
     HotelService hotelService;
+    @Autowired
+    PropertyTypeRepository propertyTypeRepository;
+
     @GetMapping("/hotel_detail{id}")
-    public String hotelDetail(@PathVariable("id") Long id, Model model){
+    public String hotelDetail(@PathVariable("id") Long id, Model model) {
         Optional<Hotel> hotel = hotelService.findById(id);
-        if(hotel.isPresent()){
-            model.addAttribute("background_image",hotel.get().getImages().get(0).getImage());
-            model.addAttribute("hotel",hotel.get());
+        if (hotel.isPresent()) {
+            model.addAttribute("background_image", hotel.get().getImages().get(0).getImage());
+            model.addAttribute("hotel", hotel.get());
             return "hotel_details/index";
         }
         return "redirect:/";
     }
 
     @PostMapping("/getRoomsHotelDetail")
-    public ModelAndView getRooms_hotel_detail(@RequestParam("start_date")String start_date,
-                                              @RequestParam("end_date")String end_date,
-                                              @RequestParam("number_of_people")int number_of_people,
-                                              @RequestParam("hotel_name")String hotel_name
-    ){
+    public ModelAndView getRooms_hotel_detail(@RequestParam("start_date") String start_date,
+                                              @RequestParam("end_date") String end_date,
+                                              @RequestParam("number_of_people") int number_of_people,
+                                              @RequestParam("hotel_name") String hotel_name
+    ) {
         Optional<Hotel> find_hotel = null;
-        if(hotel_name != "")
-        find_hotel = hotelService.findHotelByName(hotel_name);
+        if (hotel_name != "")
+            find_hotel = hotelService.findHotelByName(hotel_name);
         Hotel hotel;
-        ModelAndView mv = new ModelAndView("hotel_details/rooms_hotel_detail");
-        if(find_hotel.isPresent()){
+        ModelAndView mv = new ModelAndView("hotel_details/rooms_hotel_detail2");
+        if (find_hotel.isPresent()) {
             hotel = find_hotel.get();
             List<Room> rooms = hotel.getRooms();
-            if(rooms!=null) {
-                rooms = sortByPrice(rooms,"ASC");
-                if(start_date.equals("no-date"))
-                mv.addObject("rooms", rooms);
-                else{
-                    rooms= filterByDateBookingAndNumberOfPeople(rooms,start_date,end_date,number_of_people);
-                    mv.addObject("rooms", rooms);
-                }
+//            if (rooms != null) {
+//                rooms = sortByPrice(rooms, "ASC");
+//
+//                rooms = filterByDateBookingAndNumberOfPeople(rooms, start_date, end_date, number_of_people);
+//                mv.addObject("rooms", rooms);
+//
+//            }
+            if(rooms!=null){
+                List<RoomGroup> roomGroups = getAllFilteredRoomGroup(hotel.getId()
+                        ,start_date,end_date,number_of_people);
+                mv.addObject("roomGroups", roomGroups);
+
             }
 
 
-
         }
-
 
 
         return mv;
 
     }
-    public RoomGroup getFilteredRoomGroup(int hotel_id, String start_date, String end_date, int number_of_people){
-        RoomGroup rg = new RoomGroup();
+
+    // co the return null neu hotel_id sai
+    public List<RoomGroup> getAllFilteredRoomGroup(Long hotel_id, String start_date, String end_date, int number_of_people) {
+        List<RoomGroup> roomGroups = new ArrayList<>();
+        RoomGroup roomGroup = null;
+        Iterable<PropertyType> types = propertyTypeRepository.findAll();
+        for (PropertyType propertyType : types) {
+            roomGroup = getFilteredRoomGroup(hotel_id, start_date, end_date, number_of_people, propertyType);
+            if (roomGroup != null) {
+                roomGroups.add(roomGroup);
+            }
+
+
+        }
+        if (roomGroups.isEmpty()) return null;
+        return roomGroups;
+    }
+
+    // co the return null neu khach san khong ton tai hoac khong co phong thich hop
+    public RoomGroup getFilteredRoomGroup(Long hotel_id, String start_date, String end_date, int number_of_people, PropertyType type) {
+        RoomGroup rg = null;
+
+        Optional<Hotel> hotel = hotelService.findById(hotel_id);
+
+        if (hotel.isPresent()) {
+            List<Room> rooms = hotel.get().getRooms();
+            if (!rooms.isEmpty()) {
+                rooms = filterByDateBookingAndNumberOfPeople(rooms, start_date, end_date, number_of_people);
+                rooms = filterByRoomType(rooms, type);
+                if (!rooms.isEmpty()) {
+                    rg = new RoomGroup(rooms, rooms.get(0));
+                }
+            }
+        }
+
         return rg;
 
     }
-    public List<Room> filterByDateBookingAndNumberOfPeople(List<Room> roomList, String start_date, String end_date,int number_of_people){
 
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        dtf = dtf.withLocale(Locale.getDefault());
-        LocalDate start_dateD = LocalDate.now();
-        LocalDate end_dateD = LocalDate.now();
+    public List<Room> filterByRoomType(List<Room> roomList, PropertyType type) {
+        List<Room> filteredRooms = new ArrayList<>();
+        for (Room room : roomList) {
 
-
-        for (int i = roomList.size() - 1 ; i>=0 ; i--) {
-            if (roomList.get(i).getTotalOfBedroom() < number_of_people) {
-                roomList.remove(i);
+            if (room.getPropertyType().getId() == type.getId()) {
+                filteredRooms.add(room);
             }
         }
-        try {
-            start_dateD = LocalDate.parse(start_date);
-            end_dateD = LocalDate.parse(end_date);
-        for(int i = roomList.size() -1 ; i >=0 ; i--){
-            List<Booking> bookings = roomList.get(i).getBookings();
-            boolean isValid = true;
-            if(!bookings.isEmpty()){
-                for (int j = 0 ; j<bookings.size();j++){
-                    if(bookings.get(j).getStartDate().isAfter(end_dateD)||bookings.get(j).getEndDate().isBefore(start_dateD)){
-                       // thoa man khong lam gi
-                    }else{
-                        isValid = false;
-                        break;
+        if (filteredRooms.isEmpty()) return null;
+        return filteredRooms;
+    }
+
+    public List<Room> filterByDateBookingAndNumberOfPeople(List<Room> roomList, String start_date, String end_date, int number_of_people) {
+        if (!start_date.equals("no-date")) {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            dtf = dtf.withLocale(Locale.getDefault());
+            LocalDate start_dateD = LocalDate.now();
+            LocalDate end_dateD = LocalDate.now();
+
+
+            for (int i = roomList.size() - 1; i >= 0; i--) {
+                if (roomList.get(i).getTotalOfBedroom() < number_of_people) {
+                    roomList.remove(i);
+                }
+            }
+            try {
+                start_dateD = LocalDate.parse(start_date);
+                end_dateD = LocalDate.parse(end_date);
+                for (int i = roomList.size() - 1; i >= 0; i--) {
+                    List<Booking> bookings = roomList.get(i).getBookings();
+                    boolean isValid = true;
+                    if (!bookings.isEmpty()) {
+                        for (int j = 0; j < bookings.size(); j++) {
+                            if (bookings.get(j).getStartDate().isAfter(end_dateD) || bookings.get(j).getEndDate().isBefore(start_dateD)) {
+                                // thoa man khong lam gi
+                            } else {
+                                isValid = false;
+                                break;
+                            }
+                        }
+
+                    }
+                    if (isValid == false) {
+                        roomList.remove(i);
                     }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
 
             }
-            if(isValid == false){
-                roomList.remove(i);
-            }
-        }
-        }catch (Exception e){
-            e.printStackTrace();
-            System.out.println("loi ngay thang nhap vao");
         }
         return roomList;
     }
-    private List<Room> sortByPrice(List<Room> roomList, String order){
-        final int orderN ;
-        if(order.equals("DESC"))
+
+    private List<Room> sortByPrice(List<Room> roomList, String order) {
+        final int orderN;
+        if (order.equals("DESC"))
             orderN = -1;
         else orderN = 1;
-        Collections.sort(roomList,new Comparator<Room>(){
+        Collections.sort(roomList, new Comparator<Room>() {
 
             @Override
             public int compare(Room r1, Room r2) {
-                if(r1.getPricePerNight()>r2.getPricePerNight())
-                return orderN;
+                if (r1.getPricePerNight() > r2.getPricePerNight())
+                    return orderN;
                 return -orderN;
             }
         });
--- src/main/java/com/example/bookinghotel/entities/RoomGroup.java
+++ src/main/java/com/example/bookinghotel/entities/RoomGroup.java
@@ -3,37 +3,29 @@ package com.example.bookinghotel.entities;
 import java.util.List;
 
 public class RoomGroup {
-    String type ;
-    int length_of_room_group;
-    List<Room> empty_rooms;
-    double price;
 
-    public RoomGroup(String type, int length_of_room_group, List<Room> empty_rooms, double price) {
-        this.type = type;
-        this.length_of_room_group = length_of_room_group;
+
+    private List<Room> empty_rooms;
+    private Room specificRoom;
+
+
+    public RoomGroup(List<Room> empty_rooms, Room specificRoom) {
         this.empty_rooms = empty_rooms;
-        this.price = price;
+        this.specificRoom = specificRoom;
+    }
+
+    public Room getSpecificRoom() {
+        return specificRoom;
+    }
+
+    public void setSpecificRoom(Room specificRoom) {
+        this.specificRoom = specificRoom;
     }
 
     public RoomGroup() {
 
     }
 
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getLength_of_room_group() {
-        return length_of_room_group;
-    }
-
-    public void setLength_of_room_group(int length_of_room_group) {
-        this.length_of_room_group = length_of_room_group;
-    }
 
     public List<Room> getEmpty_rooms() {
         return empty_rooms;
@@ -43,11 +35,4 @@ public class RoomGroup {
         this.empty_rooms = empty_rooms;
     }
 
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
 }
