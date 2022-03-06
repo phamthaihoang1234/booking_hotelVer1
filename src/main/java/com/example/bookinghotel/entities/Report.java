4eaaddd28dbaeb91c94229e438c840bf3858641b
Quangtmhe151445 <quangtmhe151445@fpt.edu.vn>
2022-03-06 18:38:57 +0700

    update Report entities

--- src/main/java/com/example/bookinghotel/entities/Report.java
+++ src/main/java/com/example/bookinghotel/entities/Report.java
@@ -11,9 +11,74 @@ public class Report {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column
     private String name;
+    private String email;
     private String message;
-    private int hotel_id;
 
+    @ManyToOne
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
 
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public Report(int id, String name, String email, String message, Hotel hotel, Room room) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.message = message;
+        this.hotel = hotel;
+        this.room = room;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
 }
