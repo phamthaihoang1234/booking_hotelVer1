d4a5781d54f47c6d3f1bdcddebd67cf8ff8f1424
Quangtmhe151445 <quangtmhe151445@fpt.edu.vn>
2022-03-15 23:28:28 +0700

    update search in hotel_report

--- src/main/java/com/example/bookinghotel/controller/HotelReportController.java
+++ src/main/java/com/example/bookinghotel/controller/HotelReportController.java
@@ -8,6 +8,7 @@ import com.example.bookinghotel.services.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -66,9 +67,21 @@ public class HotelReportController {
 
 
     @GetMapping("/hotelReportList")
-    public String hotelReportListOwner(Model model){
+    public String hotelReportListOwner(Model model,
+                                       @RequestParam(name = "nameOfhotel",required = false) String nameOfhotel,
+                                       @RequestParam(name = "message",required = false) String message){
 
-        model.addAttribute("report",hotelReportRepository.findAll());
+        Iterable<Report> reports = null;
+        if(StringUtils.hasText(nameOfhotel) && StringUtils.hasText(message)){
+            reports = hotelReportRepository.findByNameOfHotelAndMessage(nameOfhotel,message);
+        }else if(StringUtils.hasText(nameOfhotel)){
+            reports = hotelReportRepository.findByNameOfHotel(nameOfhotel);
+        }else if(StringUtils.hasText(message)){
+            reports = hotelReportRepository.findByMessage(message);
+        }else{
+            reports = hotelReportRepository.findAll();
+        }
+        model.addAttribute("report",reports);
 
         return "/Pages/Admin/hotel_report_list";
     }
